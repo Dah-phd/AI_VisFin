4a747e4e32381e998dabdd7d8f64752d0dce46bd
Daniel Nikolaev <toolsproed@gmail.com>
2021-08-04 20:49:57 +0300

    better class naem

--- modules/data_feed.py
+++ modules/data_feed.py
@@ -8,13 +8,35 @@ else:
 from numpy import array, std, log
 
 
-class prepare_data():
+class PrepareData():
     def __init__(self, database, table):
         self.db = DATABASE(database, table)
         self.full_data = export(self.db)
         self.keys = list(self.full_data.keys())[1:]
 
     def make_graph(self, key, start=0, step=5, horizon=42, width=200):
+        """
+        key, str, is a key from self.keys;
+        start, int, where to start in the key value;
+        step, int, is the offset from the initial period used for category;
+        horizon is the data made into graph;
+        width, int, the width of the generated grid graph;
+        """
+        try:
+            data = self.full_data[key][start:start+step+horizon+1]
+        except IndexError:
+            return 'Finished'
+        data = data[step:]
+        data = log(array(data[:-1])/array(data[1:]))[::-1]
+        data_categorical = sum(data[0:step])
+        base_categorical = std(data)
+        category = self._categorize(data_categorical, base_categorical)
+        data = self._restructure(data, width)
+        if not data:
+            return False
+        return (data, category, base_categorical)
+
+    def test_data_graph(self, key, start=0, step=5, horizon=42, width=200):
         """
         key, str, is a key from self.keys;
         start, int, where to start in the key value;
